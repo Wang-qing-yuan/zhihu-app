461673e853f84d8d0bfbacef902177b8bdec7838
wang-qing-yuan <1950016253@qq.com>
2020-01-20 21:02:53 +0800

    后端3.0

---
+++ src/main/java/com/scs/soft/zhihu/api/controller/FavoriteController.java
@@ -0,0 +1,33 @@
+package com.scs.soft.zhihu.api.controller;
+
+import com.scs.soft.zhihu.api.common.Result;
+import com.scs.soft.zhihu.api.service.FavoriteService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @ClassName FavoriteController
+ * @Description TODO
+ * @Author wangqingyuan
+ * @Date 2020/1/19 &19:40
+ * @Version 1.0
+ **/
+@RestController
+@RequestMapping(value = "/api/favorite")
+public class FavoriteController {
+    @Resource
+    private FavoriteService favoriteService;
+
+    @GetMapping
+    public Result getRecentFavorite(){
+        return  Result.success(favoriteService.selectRecentFavorite());
+    }
+
+    @GetMapping(value = "/all")
+    public Result getAllRoundTable(){
+        return  Result.success(favoriteService.selectAllFavorite());
+    }
+}
